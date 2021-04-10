8de05af5558856a4bcbe66e2769d6583e7b2b2cb
spetseffect <spetseffect@gmail.com>
2021-04-11 00:27:29 +0300

    Palindrom

--- src/main/java/com/hw/task3/StrFunc.java
+++ src/main/java/com/hw/task3/StrFunc.java
@@ -1,9 +1,13 @@
 package com.hw.task3;
 
 public class StrFunc {
-    public boolean Palindrom(String str){}
-    public Integer VowelCount(String str){}
-    public Integer ConsonantCount(String str){}
-    public Integer SubstringCount(String str){}
-
+    public static boolean Palindrom(String str){
+        var len=str.length();
+        for(int i=0,j=len-1;i<j;i++,j--)
+            if(str.charAt(i)!=str.charAt(j)) return false;
+        return true;
+    }
+//    public static Integer VowelCount(String str){}
+//    public static Integer ConsonantCount(String str){}
+//    public static Integer SubstringCount(String str){}
 }
