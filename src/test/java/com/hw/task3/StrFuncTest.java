6cedfafe677082bb9b372e961213664b878f3468
spetseffect <spetseffect@gmail.com>
2021-04-11 02:06:40 +0300

    StrFuncTest init

---
+++ src/test/java/com/hw/task3/StrFuncTest.java
@@ -0,0 +1,16 @@
+package com.hw.task3;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName(value = "Testing StrFunc.")
+public class StrFuncTest {
+    @Test
+    public void testPalindrom(){}
+    @Test
+    public void testVowelCount(){}
+    @Test
+    public void testConsonantCount(){}
+    @Test
+    public void testWordCount(){}
+}
