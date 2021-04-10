d56ff7ff11f7f16700dbd529507148cf2e707ea0
spetseffect <spetseffect@gmail.com>
2021-04-10 20:05:34 +0300

    class Triangle

---
+++ src/main/java/com/hw/junit/Triangle.java
@@ -0,0 +1,39 @@
+package com.hw.junit;
+
+import java.util.Objects;
+
+public class Triangle implements IFigure{
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public Double getArea() {
+        double p=(a+b+c)/2;
+        return p*Math.sqrt(p*(p-a)*(p-b)*(p-c));
+    }
+    public boolean check(){
+        return a+b>c && a+c>b && b+c>a;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return Double.compare(triangle.a, a) == 0
+                && Double.compare(triangle.b, b) == 0
+                && Double.compare(triangle.c, c) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b, c);
+    }
+}
