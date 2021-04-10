d0a747ddbab8e1f96724fc2ec6077182c9dea941
spetseffect <spetseffect@gmail.com>
2021-04-10 21:00:32 +0300

    class Square

---
+++ src/main/java/com/hw/junit/Square.java
@@ -0,0 +1,29 @@
+package com.hw.junit;
+
+import java.util.Objects;
+
+public class Square implements IFigure{
+    private double a;
+
+    public Square(double a) {
+        this.a = a;
+    }
+
+    @Override
+    public Double getArea() {
+        return a*a;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Square square = (Square) o;
+        return Double.compare(square.a, a) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a);
+    }
+}
