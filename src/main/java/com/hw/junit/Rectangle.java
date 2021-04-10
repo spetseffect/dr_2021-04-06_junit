d7dca19d85ab51bd69c65e9a1332fb8a9a6abf1e
spetseffect <spetseffect@gmail.com>
2021-04-10 20:18:11 +0300

    class Rectangle

---
+++ src/main/java/com/hw/junit/Rectangle.java
@@ -0,0 +1,31 @@
+package com.hw.junit;
+
+import java.util.Objects;
+
+public class Rectangle implements IFigure{
+    private double a;
+    private double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public Double getArea() {
+        return a*b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return Double.compare(rectangle.a, a) == 0 && Double.compare(rectangle.b, b) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
+}
