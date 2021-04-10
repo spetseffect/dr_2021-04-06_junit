e403fdb4c0dc225f89dd4a83539dc514ae83e2eb
spetseffect <spetseffect@gmail.com>
2021-04-11 02:34:02 +0300

    testVowelCount success

--- src/test/java/com/hw/task3/StrFuncTest.java
+++ src/test/java/com/hw/task3/StrFuncTest.java
@@ -5,10 +5,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName(value = "Testing StrFunc.")
 public class StrFuncTest {
+    private static String str;
+    private static String word;
+    @BeforeAll
+    public static void init(){
+        str="Тут какя-то строка для тестирования и для очередной тренировки набора текста вслепую. " +
+                "Также тут дополнительно несколько просто наборов букв для количества: авырпдляав, тута, набор, текст.";
+        word="для";
+    }
     @Test
-    public void testPalindrom(){}
+    public void testPalindrom(){
+        boolean expected=StrFunc.Palindrom("радар rotor радар");
+        assertTrue(expected,"testPalindrom failed");
+    }
     @Test
-    public void testVowelCount(){}
+    public void testVowelCount(){
+        int expected=StrFunc.VowelCount("очередной");
+        int actual=4;
+
+    }
     @Test
     public void testConsonantCount(){}
     @Test
