6b7b65eafaa697b4facba5c2ac221f96c54aa631
spetseffect <spetseffect@gmail.com>
2021-04-10 21:06:48 +0300

    class Rhomb

---
+++ src/main/java/com/hw/junit/Rhomb.java
@@ -0,0 +1,34 @@
+package com.hw.junit;
+
+import java.util.Objects;
+
+public class Rhomb implements IFigure{
+    //диагонали ромба
+    private double a;
+    private double b;
+
+    public Rhomb(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public Double getArea() {
+        return a*b/2;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rhomb rhomb = (Rhomb) o;
+        return ((Double.compare(rhomb.a, a) == 0 && Double.compare(rhomb.b, b) == 0)
+                || (Double.compare(rhomb.b, a) == 0 && Double.compare(rhomb.a, b) == 0))
+                && a!=b;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
+}
