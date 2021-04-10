9b8fba770367ccf190a37861472f4d74d6b35425
spetseffect <spetseffect@gmail.com>
2021-04-11 00:23:36 +0300

    task3 init

---
+++ src/main/java/com/hw/task3/StrFunc.java
@@ -0,0 +1,9 @@
+package com.hw.task3;
+
+public class StrFunc {
+    public boolean Palindrom(String str){}
+    public Integer VowelCount(String str){}
+    public Integer ConsonantCount(String str){}
+    public Integer SubstringCount(String str){}
+
+}
