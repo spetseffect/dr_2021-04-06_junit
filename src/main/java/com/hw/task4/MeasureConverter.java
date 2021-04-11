9c16e3a8220026431859737c08b92b22656f552c
spetseffect <spetseffect@gmail.com>
2021-04-11 14:31:59 +0300

    public static Double getDM()

--- src/main/java/com/hw/task4/MeasureConverter.java
+++ src/main/java/com/hw/task4/MeasureConverter.java
@@ -1,10 +1,11 @@
 package com.hw.task4;
 
 public class MeasureConverter {
-    public static Double convert(double valueIn,MeasureUnits unitIn,double valueOut,MeasureUnits unitOut){
+    public static Double convert(double valueIn, MeasureUnits unitIn, double valueOut, MeasureUnits unitOut) {
         return null;
     }
-    public static Double getMM(double valueIn,MeasureUnits unitIn){
+
+    public static Double getMM(double valueIn, MeasureUnits unitIn) {
         return switch (unitIn) {
             case mm -> valueIn;
             case sm -> valueIn * 10;
@@ -13,7 +14,8 @@ public class MeasureConverter {
             case km -> valueIn * 1000000;
         };
     }
-    public static Double getSM(double valueIn,MeasureUnits unitIn){
+
+    public static Double getSM(double valueIn, MeasureUnits unitIn) {
         return switch (unitIn) {
             case mm -> valueIn / 10;
             case sm -> valueIn;
@@ -22,4 +24,15 @@ public class MeasureConverter {
             case km -> valueIn * 100000;
         };
     }
+
+    public static Double getDM(double valueIn, MeasureUnits unitIn) {
+        return switch (unitIn) {
+            case mm -> valueIn / 100;
+            case sm -> valueIn / 10;
+            case dm -> valueIn;
+            case m -> valueIn * 10;
+            case km -> valueIn * 10000;
+        };
+    }
+
 }
