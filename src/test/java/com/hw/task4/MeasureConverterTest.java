b0262ef177a1e9111512ca0a5f3582b010be2eaf
spetseffect <spetseffect@gmail.com>
2021-04-11 15:32:05 +0300

    testconvert success

--- src/test/java/com/hw/task4/MeasureConverterTest.java
+++ src/test/java/com/hw/task4/MeasureConverterTest.java
@@ -1,11 +1,16 @@
 package com.hw.task4;
 
+import com.hw.task3.StrFunc;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MeasureConverterTest {
     @Test
-    public void testconvert(){}
+    public void testconvert(){
+        double expected= MeasureConverter.convert(245,MeasureUnits.mm,MeasureUnits.sm);
+        double actual=24.5;
+        assertEquals(expected,actual,"testVowelCount failed");
+    }
     @Test
     public void testgetMM(){}
     @Test
