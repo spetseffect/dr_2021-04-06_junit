3864102c838c4b183b6efe911b71b6e767e31308
spetseffect <spetseffect@gmail.com>
2021-04-11 15:23:21 +0300

    MeasureConverterTest init

---
+++ src/test/java/com/hw/task4/MeasureConverterTest.java
@@ -0,0 +1,19 @@
+package com.hw.task4;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MeasureConverterTest {
+    @Test
+    public void testconvert(){}
+    @Test
+    public void testgetMM(){}
+    @Test
+    public void testgetSM(){}
+    @Test
+    public void testgetDM(){}
+    @Test
+    public void testgetM(){}
+    @Test
+    public void testgetKM(){}
+}
