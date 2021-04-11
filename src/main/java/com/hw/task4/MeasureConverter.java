aa28a4e483caf32e76ed90e4c0dc1b026cf6fc8f
spetseffect <spetseffect@gmail.com>
2021-04-11 15:06:27 +0300

    public static Double getKM()

--- src/main/java/com/hw/task4/MeasureConverter.java
+++ src/main/java/com/hw/task4/MeasureConverter.java
@@ -4,44 +4,49 @@ public class MeasureConverter {
     public static Double convert(double valueIn, MeasureUnits unitIn, double valueOut, MeasureUnits unitOut) {
         return null;
     }
-
     public static Double getMM(double valueIn, MeasureUnits unitIn) {
         return switch (unitIn) {
             case mm -> valueIn;
             case sm -> valueIn * 10;
             case dm -> valueIn * 100;
-            case m -> valueIn * 1000;
+            case  m -> valueIn * 1000;
             case km -> valueIn * 1000000;
         };
     }
-
     public static Double getSM(double valueIn, MeasureUnits unitIn) {
         return switch (unitIn) {
             case mm -> valueIn / 10;
             case sm -> valueIn;
             case dm -> valueIn * 10;
-            case m -> valueIn * 100;
+            case  m -> valueIn * 100;
             case km -> valueIn * 100000;
         };
     }
-
     public static Double getDM(double valueIn, MeasureUnits unitIn) {
         return switch (unitIn) {
             case mm -> valueIn / 100;
             case sm -> valueIn / 10;
             case dm -> valueIn;
-            case m -> valueIn * 10;
+            case  m -> valueIn * 10;
             case km -> valueIn * 10000;
         };
     }
-
     public static Double getM(double valueIn, MeasureUnits unitIn) {
         return switch (unitIn) {
             case mm -> valueIn / 1000;
             case sm -> valueIn / 100;
             case dm -> valueIn / 10;
-            case m -> valueIn;
+            case  m -> valueIn;
             case km -> valueIn * 1000;
+        };
+    }
+    public static Double getKM(double valueIn, MeasureUnits unitIn) {
+        return switch (unitIn) {
+            case mm -> valueIn / 1000000;
+            case sm -> valueIn / 100000;
+            case dm -> valueIn / 10000;
+            case  m -> valueIn / 1000;
+            case km -> valueIn;
         };
     }
 }
